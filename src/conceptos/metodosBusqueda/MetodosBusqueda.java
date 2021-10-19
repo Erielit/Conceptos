f0d4e76c882c7c6c51986e6ff512303eb6131eaa
Erielit <57473613+Erielit@users.noreply.github.com>
2021-10-19 14:51:45 -0500

    [UPDATE]Metodos de busqueda

--- src/conceptos/metodosBusqueda/MetodosBusqueda.java
+++ src/conceptos/metodosBusqueda/MetodosBusqueda.java
@@ -13,7 +13,7 @@ public class MetodosBusqueda {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        busquedaSecuencial(arr, 10);
+        busquedaBinaria(arr, 2);
     }
 
     public static void busquedaSecuencial(int[] arr, int num) {
